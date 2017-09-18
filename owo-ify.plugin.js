b1a0b0e6f8c798a3658ada0c2158a11d4d163def
Kalon <kaloncpu57@gmail.com>
2017-09-18 01:25:06 -0600

    Fixes for owo-ify
    Removed everything from load function, fixed issue where stop wouldn't
    work

--- owo-ify.plugin.js
+++ owo-ify.plugin.js
@@ -2,25 +2,23 @@
 
 class owoify {
   constructor() {
-    this.makeOwO = function (owo, txt) {
-      txt.value = txt.value.replace(/r|l/g, "w").replace(/R|L/g, "W");
+    this.makeOwO = function () {
+      this.value = this.value.replace(/r|l/g, "w").replace(/R|L/g, "W");
     };
   }
 
   cleanUp() {
-    document.querySelector(".channel-text-area-default textarea").removeEventListener("input", this.makeOwO);
+    let textarea = document.querySelector(".channel-text-area-default textarea");
+    textarea.removeEventListener("input", this.makeOwO);
   }
 
-  load() {
+  load() {}
+
+  start() {
     console.log("OwO what's this?");
     let self = this;
     let textarea = document.querySelector(".channel-text-area-default textarea");
-    textarea.addEventListener("input", self.makeOwO.bind(textarea, self, textarea));
-  }
-  start() {
-    let self = this;
-    let textarea = document.querySelector(".channel-text-area-default textarea");
-    textarea.addEventListener("input", self.makeOwO.bind(textarea, self, textarea));
+    textarea.addEventListener("input", self.makeOwO);
   }
 
   stop() {
@@ -33,6 +31,6 @@ class owoify {
 
   getName        () { return "owo-ify"; }
   getDescription () { return "Replaces all l's and r's in your own messages with w's"; }
-  getVersion     () { return "0.0.1"; }
+  getVersion     () { return "0.1.0"; }
   getAuthor      () { return "kaloncpu57"; }
 }
