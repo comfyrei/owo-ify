4c75da2d8e0875599f41c1b068588fbb99f5b0d3
Kalon <kaloncpu57@gmail.com>
2018-01-09 03:42:39 -0700

    Update owo-ify for Discord update
    Hopefully future proofed this plugin pretty well, but will update again
    as necessary

--- owo-ify.plugin.js
+++ owo-ify.plugin.js
@@ -4,17 +4,19 @@ class owoify {
   constructor() {
     this.makeOwO = function () {
       this.value = this.value.replace(/r|l/g, "w").replace(/R|L/g, "W");
+      // let txt = this.value.replace(/r|l/g, "w").replace(/R|L/g, "W");
+      // this.value = txt;
     };
   }
 
   setup() {
     let self = this;
-    let textarea = document.querySelector(".channel-text-area-default textarea");
+    let textarea = document.querySelector('textarea');
     textarea.addEventListener("input", self.makeOwO);
   }
 
   cleanUp() {
-    let textarea = document.querySelector(".channel-text-area-default textarea");
+    let textarea = document.querySelector('textarea');
     textarea.removeEventListener("input", this.makeOwO);
   }
 
@@ -39,6 +41,6 @@ class owoify {
 
   getName        () { return "owo-ify"; }
   getDescription () { return "Replaces all l's and r's in your own messages with w's"; }
-  getVersion     () { return "0.2.0"; }
+  getVersion     () { return "0.2.2"; }
   getAuthor      () { return "kaloncpu57"; }
 }
